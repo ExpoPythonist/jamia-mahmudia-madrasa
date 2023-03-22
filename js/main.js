306bdf719b5bb67798ea41c073ef6365c36a2ae1
AshiqueAI <ashique@aidocbuilder.com>
2023-03-22 17:45:01 +0600

    Image Update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+
+
